0f97f83956730ca7e5477e7c5db0b65416ce7bbe
makehoney <makehoney3@gmail.com>
2019-06-09 02:54:18 +0900

    [Edit migration script] 주소 변수 선언

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,10 +1,11 @@
 const Doajou = artifacts.require('./Doajou.sol')
+require('dotenv').config()
 
 module.exports = deployer => {
   const _tokenName = 'Doa Token'
   const _tokenSymbol = 'DOAT'
   const _tokenDecimals = 18
-  const _managerAddress = '0x77a52a338113fC6690cFF6441C4c6d45907670Ae'
+  const _managerAddress = process.env.MANAGER_ADDRESS
 
   deployer.deploy(Doajou, _managerAddress, _tokenName, _tokenSymbol, _tokenDecimals)
 }
